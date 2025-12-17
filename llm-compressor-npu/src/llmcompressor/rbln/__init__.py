9f7b029f23a41d67fcb20989413859abda3f5a0e
Daehyun Ahn <daehyun.ahn@squeezebits.com>
2025-12-17 13:47:27 +0900

    feat: add rbln-related environment variables and subgraph compiling

---
+++ llm-compressor-npu/src/llmcompressor/rbln/__init__.py
@@ -0,0 +1,17 @@
+import functools
+import torch
+
+@functools.cache
+def is_rbln_available():
+    try:
+        import rebel
+    except Exception as e:
+        return False
+    return hasattr(torch, "rbln")
+
+if is_rbln_available():
+    from .rbln_envs import ENFORCE_EAGER, USE_CUSTOM_OPS
+    from .rbln_ops import *
+else:
+    ENFORCE_EAGER = True
+    USE_CUSTOM_OPS = False
